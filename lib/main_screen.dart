0e384fc0a2a3f02442b330cf0957ceb877ac82ee
DESKTOP-89L6R8I\hendp <hendpermana12@gmail.com>
2022-02-16 12:50:55 +0700

    make list anime ranking

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:doki_doki/model/anime_rank_model.dart';
 import 'package:flutter/material.dart';
 import 'package:doki_doki/model/manga_rank_model.dart';
 
@@ -10,7 +11,7 @@ class MainScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Doki Doki'),
         ),
-        body: MangaRankingScreen());
+        body: AnimeRankingScreen());
   }
 }
 
@@ -90,6 +91,98 @@ class MangaRankingScreen extends StatelessWidget {
                             const Icon(Icons.brush, color: Colors.blueAccent),
                             const SizedBox(width: 10),
                             Text(manga.author)
+                          ]),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: mangaRankModel.length,
+      ),
+    );
+  }
+}
+
+class AnimeRankingScreen extends StatelessWidget {
+  var titleStyle = const TextStyle(
+      fontSize: 30.0, fontWeight: FontWeight.bold, fontFamily: 'Poppins');
+  var informationTextStyle = const TextStyle(fontFamily: 'Raleway');
+
+  AnimeRankingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          final AnimeRankModel anime = animeRankModel[index];
+          return Card(
+            shadowColor: Colors.grey,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            "Rank",
+                            style: TextStyle(
+                                fontSize: 21.0, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "# " + anime.ranking.toString(),
+                            style: const TextStyle(fontSize: 32.0),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Hero(
+                        tag: "place-image-${anime.urlPicture}",
+                        child: Image.network(anime.urlPicture)),
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            anime.name,
+                            style: const TextStyle(
+                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(children: <Widget>[
+                            const Icon(Icons.star, color: Colors.amberAccent),
+                            const SizedBox(width: 10),
+                            Text(anime.score.toString())
+                          ]),
+                          const SizedBox(height: 10),
+                          Row(children: <Widget>[
+                            const Icon(Icons.movie_creation, color: Colors.blueAccent),
+                            const SizedBox(width: 10),
+                            Text(anime.studio)
                           ]),
                         ],
                       ),
